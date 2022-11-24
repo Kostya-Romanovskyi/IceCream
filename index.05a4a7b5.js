f4d3a09ae21a7e899cc8d93c03c81e9dd1173d37
Kostya-Romanovskyi <106028051+Kostya-Romanovskyi@users.noreply.github.com>
2022-11-24 14:24:58 +0000

    Deploying to gh-pages from @ Kostya-Romanovskyi/IceCream-layout@a9494655720a0b2cdfdb4c325b183b3f548c196c 🚀

--- index.05a4a7b5.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0},simulateTouch:!1,keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},autoheight:!0});
-//# sourceMappingURL=index.05a4a7b5.js.map
