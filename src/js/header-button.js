8d28c55d941188c326f71715dd8f014cb694f8bb
Kostiantyn Romanovskyi <romanovskiyk97@gmail.com>
2022-12-10 14:11:01 +0200

    hero-tablet

--- src/js/header-button.js
+++ src/js/header-button.js
@@ -1,12 +1,9 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-open]");
+  const menuBtnRef = document.querySelector('[data-menu-open]');
+  const tabletBtnRef = document.querySelector('.tablet__btn');
 
-
-    menuBtnRef.addEventListener("click", () => {
-
-        menuBtnRef.classList.toggle("is-open");
-
-    });
-
-
+  menuBtnRef.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('is-open');
+    tabletBtnRef.classList.toggle('tablet__btn--none');
+  });
 })();
