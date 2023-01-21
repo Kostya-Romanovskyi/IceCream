94c0b318ee08a17d8ef4a1133888c4439d90af0f
Kostiantyn Romanovskyi <romanovskiyk97@gmail.com>
2023-01-21 15:11:57 +0200

    added animations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import "./js/swiper";
-import "./js/header-burger";
-import "./js/header-button";
-import "./js/header-modal";
-import "./js/other-modals";
-
+import './js/swiper';
+import './js/header-burger';
+import './js/header-button';
+import './js/header-modal';
+import './js/other-modals';
