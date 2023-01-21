52094892eca4cbd11a6798cb2586bb425abbbb01
Kostiantyn Romanovskyi <romanovskiyk97@gmail.com>
2023-01-21 17:06:27 +0200

    add many functional

--- src/js/header-burger.js
+++ src/js/header-burger.js
@@ -9,10 +9,10 @@
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    // const scrollLockMethod = !isMenuOpen
-    //   ? 'disableBodyScroll'
-    //   : 'enableBodyScroll';
-    // bodyScrollLock[scrollLockMethod](document.body);
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
@@ -26,3 +26,37 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+const home = document.querySelector('[name="home"]');
+const howItMade = document.querySelector('[name="how-it`s-made"]');
+const ourProducts = document.querySelector('[name="our-products"]');
+const contacts = document.querySelector('[name="contacts"]');
+const wrap = document.getElementById('header__mobile-wrapp');
+const btnMenuOpen = document.querySelector('[data-menu-open]');
+const mainBtn = document.getElementById('modal-buy-now-open');
+
+home.addEventListener('click', onRemoveListHome);
+howItMade.addEventListener('click', onRemoveListMade);
+ourProducts.addEventListener('click', onRemoveListProducts);
+contacts.addEventListener('click', onRemoveListContacts);
+
+function onRemoveListHome() {
+  wrap.classList.remove('is-open');
+  btnMenuOpen.classList.remove('is-open');
+  mainBtn.classList.remove('tablet__btn--none');
+}
+function onRemoveListMade() {
+  wrap.classList.remove('is-open');
+  btnMenuOpen.classList.remove('is-open');
+  mainBtn.classList.remove('tablet__btn--none');
+}
+function onRemoveListProducts() {
+  wrap.classList.remove('is-open');
+  btnMenuOpen.classList.remove('is-open');
+  mainBtn.classList.remove('tablet__btn--none');
+}
+function onRemoveListContacts() {
+  wrap.classList.remove('is-open');
+  btnMenuOpen.classList.remove('is-open');
+  mainBtn.classList.remove('tablet__btn--none');
+}
